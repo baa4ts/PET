b9607fa8831a10a670c19c0e95f806469a863e15
baa4ts <baa4tsdev@gmail.com>
2026-04-15 15:15:30 -0300

    Update config, mejor rendimiento

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import 'vite-automatized-config'
 
 // https://vite.dev/config/
 export default defineConfig({
